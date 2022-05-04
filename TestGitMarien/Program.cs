84301a1a3ba24d091be3c5327af040944d517522
Marien Mehillka <S4554200@studenti.unige.it>
2022-05-04 13:05:43 +0200

    Aggiungere i file di progetto.

---
+++ TestGitMarien/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestGitMarien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bella");
+        }
+    }
+}
